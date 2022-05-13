77a7deb346b07e7545ed96238ef9f058d117e80c
dvqm <10248779+dvqm@users.noreply.github.com>
2022-05-13 15:53:46 +0200

    Shelve() => Model(). Added: setDate(), setId(), getLibrary(), addBook().

--- src/Model.js
+++ src/Model.js
@@ -1,15 +1,49 @@
 import data from './data.json';
 
-class Shelve {
-  constructor(myLibrary, deletedBooks) {
-    this.myLibrary = myLibrary;
-    this.deletedBooks = deletedBooks;
+class Model {
+  static setId(book) {
+    const library = this.getLibrary();
+    const bookCopy = book;
+    bookCopy.id = `mb-${library.length + 1}`;
+    return bookCopy;
   }
 
-  plugBook() {
+  static setDate(book) {
+    const curDate = new Date();
+    const bookCopy = book;
+    bookCopy.created = `${curDate.getFullYear()} -${curDate.getMonth()} -${
+      curDate.getDay
+    } : ${curDate.getHours()} -${curDate.getMinutes()}`;
+    return bookCopy;
+  }
+
+  static getLibrary() {
+    const storedLibrary = localStorage.getItem('library');
+    let library = JSON.parse(storedLibrary);
+    if (!library) library = [];
+    return library;
+  }
+
+  get library() {
+    return this.constructor.getLibrary();
+  }
+
+  addBook(book) {
+    const library = this.constructor.getLibrary();
+    this.constructor.setId(book);
+    this.constructor.setDate(book);
+    library.push(book);
+    const libToString = JSON.stringify(library);
+    localStorage.setItem('library', libToString);
+  }
+
+  plugBook(count) {
     const bookData = data.exampleBook;
-    this.myLibrary.push(bookData);
+    for (let i = 0; i < count; i++) {
+      this.addBook(bookData);
+    }
+    console.log(JSON.parse(localStorage.getItem('library')));
   }
 }
 
-export default Shelve;
+export default Model;
