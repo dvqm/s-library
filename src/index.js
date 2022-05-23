1d3bb6c6e70ccabe7e83a904b8796e91f795a3e6
dvqm <10248779+dvqm@users.noreply.github.com>
2022-05-23 16:00:24 +0200

    index.js minor fix of reference

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import UI from './UI';
+import UI from './ui';
+
 import Book from './Book';
+
 import Model from './Model';
+
 import UiCreator from './UiCreator';
 
 const ui = new UI();
+
 const tools = new Model();
+
 // localStorage.removeItem('library');
 
 const bookEvents = (node, book) => {
@@ -12,7 +17,9 @@ const bookEvents = (node, book) => {
 
   const getIndex = (e) => {
     const poinedCard = e.path.filter((el) => el === card)[0];
+
     const index = [...node.shell.children].indexOf(poinedCard);
+
     return index;
   };
 
@@ -20,36 +27,48 @@ const bookEvents = (node, book) => {
     if (e.path.some((el) => el.id === 'table')) {
       return i + n;
     }
+
     return i;
   };
 
   const isReadAction = (e) => {
     const newBook = { ...book };
+
     newBook.isRead = !newBook.isRead;
 
     let index = getIndex(e);
 
     const updatedBook = new UiCreator(node.shell, bookEvents(node, newBook));
+
     updatedBook.insert(index);
 
     index = indexOffset(e, index, -1);
+
     tools.update(newBook, index);
   };
 
   const isRead = card.querySelector('.isRead');
+
   isRead.addEventListener('click', isReadAction);
 
   const deleteAction = (e) => {
     let index = getIndex(e);
+
     const deletedBook = new UiCreator(node.shell, bookEvents(node, book));
+
     deletedBook.remove(index);
+
     index = indexOffset(e, index, -1);
+
     tools.remove(index);
   };
 
   const actionBtns = card.querySelectorAll('.actionBtn');
+
   const editBtn = actionBtns[0];
+
   const deleteBtn = actionBtns[1];
+
   deleteBtn.addEventListener('click', deleteAction);
 
   return card;
@@ -57,12 +76,15 @@ const bookEvents = (node, book) => {
 
 const modelView = (node) => {
   const view = node.shell;
+
   const { library } = tools;
 
   library.map((b) => {
     const book = bookEvents(node, b);
+
     return view.append(book);
   });
+
   return view;
 };
 
@@ -71,15 +93,21 @@ const settings = (node) => {
 
   const cardSet = () => {
     const sibling = document.querySelector('.statistic');
+
     sibling.nextSibling.remove();
+
     ui.view = ui.cardView;
+
     sibling.after(modelView(ui.view));
   };
 
   const tableSet = () => {
     const sibling = document.querySelector('.statistic');
+
     sibling.nextSibling.remove();
+
     ui.view = ui.tableView;
+
     sibling.after(modelView(ui.view));
   };
 
@@ -89,16 +117,20 @@ const settings = (node) => {
 
   const openDropdown = () => {
     target.after(node.dropdown);
+
     node.dropdown.addEventListener('mouseleave', closeDropdown, { once: true });
 
     const cardBtn = node.dropdown.querySelector('#toggleCard');
+
     cardBtn.addEventListener('click', cardSet);
 
     const tableBtn = node.dropdown.querySelector('#toggleTable');
+
     tableBtn.addEventListener('click', tableSet);
   };
 
   target.addEventListener('mouseenter', openDropdown);
+
   return node.wrap;
 };
 
@@ -107,24 +139,34 @@ const addBookForm = (node) => {
 
   const close = () => {
     node.close();
+
     bookBtn.render();
+
     const book = new Book();
+
     book.clear();
   };
 
   const save = () => {
     const newBook = new Book();
+
     tools.add(newBook);
+
     const book = new UiCreator(ui.view.shell, bookEvents(ui.view, newBook));
+
     book.render();
+
     close();
   };
 
   const closeBtn = node.querySelector('#close');
+
   closeBtn.addEventListener('click', close);
+
   node.addEventListener('cancel', close);
 
   const saveBtn = node.querySelector('#save');
+
   saveBtn.addEventListener('click', save);
 
   return node;
@@ -133,20 +175,29 @@ const addBookForm = (node) => {
 const addBookBtn = (node) => {
   const openForm = () => {
     const form = document.querySelector('#form');
+
     form.showModal();
+
     node.remove();
   };
 
   node.addEventListener('click', openForm);
+
   return node;
 };
 
 const mainPage = new UiCreator(
   ui.library,
+
   settings(ui.settings),
+
   ui.statistic,
+
   modelView(ui.view),
+
   addBookForm(ui.addBookForm),
+
   addBookBtn(ui.addBookBtn)
 );
+
 mainPage.render();
