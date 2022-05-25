328d0e6336fe9f17f1ae73f8552aca40e5fcc2cf
dvqm <10248779+dvqm@users.noreply.github.com>
2022-05-25 02:34:20 +0200

    Model.statistic added

--- src/Model.js
+++ src/Model.js
@@ -1,47 +1,81 @@
 class Model {
   static setDate(book) {
     const curDate = new Date();
-    const bookCopy = book;
-    bookCopy.created = `${curDate.getFullYear()}-${curDate.getMonth()}-${curDate.getDay()}`;
-    return bookCopy;
-  }
 
-  static getLibrary() {
-    const storedLibrary = localStorage.getItem('library');
-    let library = JSON.parse(storedLibrary);
-    if (!library) library = [];
-    return library;
+    const bookCopy = book;
+
+    bookCopy.created = `${curDate.getFullYear()}-${curDate.getMonth()}-${curDate.getDay()}`;
+
+    return bookCopy;
   }
 
   set library(newLibrary) {
     this.lib = JSON.stringify(newLibrary);
+
     localStorage.setItem('library', this.lib);
   }
 
   get library() {
-    return this.constructor.getLibrary();
+    const storedLibrary = localStorage.getItem('library');
+
+    let library = JSON.parse(storedLibrary);
+
+    if (!library) library = [];
+
+    this.library = library;
+
+    return library;
+  }
+
+  get statistic() {
+    const { library } = this;
+
+    const books = library.length;
+
+    const read = library.filter((book) => book.isRead === true).length;
+
+    const reducer = (prev, next) => {
+      let converted = parseInt(next.pages, 10);
+
+      if (!next.pages) converted = 0;
+
+      return prev + converted;
+    };
+
+    const pages = library.reduce(reducer, 0);
+
+    return { books, read, pages };
   }
 
   remove(index) {
-    const library = this.constructor.getLibrary();
+    const { library } = this;
+
     library.splice(index, 1);
+
     this.library = library;
   }
 
   update(newBook, index) {
-    const library = this.constructor.getLibrary();
+    const { library } = this;
+
     const newBookEntries = Object.entries(newBook);
+
     newBookEntries.forEach((prop) => {
       library[index][prop[0]] = prop[1];
     });
+
     this.library = library;
   }
 
   add(book) {
-    const library = this.constructor.getLibrary();
+    const { library } = this;
+
     this.constructor.setDate(book);
+
     library.push(book);
+
     const libToString = JSON.stringify(library);
+
     localStorage.setItem('library', libToString);
   }
 }
