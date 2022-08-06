8f01a1142968a5d36e3c0c39f9128a74d5124e14
dvqm <10248779+dvqm@users.noreply.github.com>
2022-08-06 22:52:05 +0200

    class UiCreator modified. composer(), nodeCreate() methods created. render() method modified.

--- src/UiCreator.js
+++ src/UiCreator.js
@@ -1,25 +1,42 @@
 class UiCreator {
-  constructor(node, ...rest) {
-    this.node = node;
-    this.rest = [...rest];
+  static objCopy(obj) {
+    return JSON.parse(JSON.stringify(obj));
   }
 
-  render() {
-    return this.rest.forEach((i) => {
-      this.node.append(i);
+  composer(data, ...handlers) {
+    const obj = this.constructor.objCopy(data);
+    return [...handlers].reduce((prev, next) => next(prev), obj);
+  }
+
+  nodeCreate(uiData) {
+    const element = this.constructor.objCopy(uiData);
+
+    const { tag } = element;
+
+    const parent = document.createElement(tag);
+
+    Object.entries(element).forEach((prop) => {
+      const [key, value] = prop;
+
+      if (key !== 'tag' || key !== 'c' || key !== 's') parent[key] = value;
+      if (key.includes('data-')) parent.setAttribute(key, value);
     });
+
+    const blank = this.constructor.objCopy(uiData);
+
+    if (blank.c) {
+      const { c } = blank;
+
+      Object.values(c).forEach((node) => {
+        parent.append(this.nodeCreate(node));
+      });
+    }
+
+    return parent;
   }
 
-  insert(index) {
-    const elements = this.node.childNodes;
-    return this.rest.forEach((i) => {
-      this.node.replaceChild(i, elements[index]);
-    });
-  }
-
-  remove(index) {
-    const elements = this.node.childNodes;
-    elements[index].remove();
+  render(pointer, node) {
+    return pointer.append(node);
   }
 }
 
