7ff7c8afe0abe213e797068e352585064b6323ad
dvqm <10248779+dvqm@users.noreply.github.com>
2022-08-31 01:38:46 +0200

    Book.js restructure. Created clear() and create() methods.

--- src/Book.js
+++ src/Book.js
@@ -1,44 +1,31 @@
-import data from './data.json';
-
 class Book {
-  constructor() {
-    const getFormData = (id, property) => {
-      const element = document.getElementById(id);
-      this[id] = element[property];
-    };
+  constructor(node) {
+    this.inputs = node.querySelectorAll('.addBookForm label input');
+  }
 
-    const field = data.addBookForm.field.inputData;
+  create() {
+    const book = {};
 
-    getFormData(field.author.id, 'value');
-    getFormData(field.title.id, 'value');
-    getFormData(field.pages.id, 'value');
-    getFormData(field.publish.id, 'value');
-    getFormData(field.start.id, 'value');
-    getFormData(field.finish.id, 'value');
-    getFormData(field.isRead.id, 'checked');
+    this.inputs.forEach((input) => {
+      if (input.id === 'isRead' && input.checked) {
+        book[input.id] = 'true';
+      } else if (input.id === 'isRead' && !input.checked) {
+        book[input.id] = 'false';
+      } else {
+        book[input.id] = input.value;
+      }
+    });
+
+    return book;
   }
 
   clear() {
-    const wrapId = data.addBookForm.wrap.id;
-    const inputData = data.addBookForm.field.input.tag;
-    const form = document.querySelectorAll(`#${wrapId} ${inputData}`);
-    form.forEach((child) => {
-      const input = child;
-      input.value = '';
-      input.checked = false;
-    });
-  }
+    this.inputs.forEach((input) => {
+      const field = input;
 
-  edit(book) {
-    const pocket = data.viewModels.common;
-    const fields = book.querySelectorAll(`.${pocket.input.className}`);
-    this.author = fields[0].value;
-    this.title = fields[1].value;
-    this.pages = fields[2].value;
-    this.publish = fields[3].value;
-    this.start = fields[4].value;
-    this.finish = fields[5].value;
-    this.isRead = fields[6].checked;
+      if (field.id === 'isRead') field.checked = false;
+      else field.value = '';
+    });
   }
 }
 
