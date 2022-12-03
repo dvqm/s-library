ad923c984376917dbc68a816ffa2988c56b5d830
dvqm <10248779+dvqm@users.noreply.github.com>
2022-12-02 21:53:16 -0700

    Prepared index.js for production.

--- src/index.js
+++ src/index.js
@@ -4,36 +4,3 @@ import './styles/ui.scss';
 const uiWithEvents = new Events();
 
 uiWithEvents.start();
-
-const mockLibrary = [
-  {
-    author: 'John Smith',
-    title: 'Best seller',
-    publish: '2022-01-01',
-    pages: '367',
-    start: '2022-01-02',
-    finish: '2022-01-03',
-    isRead: 'true',
-  },
-
-  {
-    author: 'Antoine de Saint-Exupéry',
-    title: 'The Little Prince',
-    publish: '1943-04-01',
-    pages: '59',
-    start: '2022-03-02',
-    // finish: '2022-01-03',
-    isRead: 'false',
-  },
-];
-
-// const model = new Model();
-// model.add(mockLibrary[0]);
-// model.add(mockLibrary[1]);
-// model.add(mockLibrary[0]);
-// model.add(mockLibrary[1]);
-// model.add(mockLibrary[0]);
-// model.add(mockLibrary[1]);
-// localStorage.removeItem('library');
-// console.log(model.library);
-// console.log(uiStructure);
